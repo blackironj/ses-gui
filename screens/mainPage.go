78e9f151071f27d676b80c0ff71d3c9ca658ce5c
blackironj <blackironj@gmail.com>
2020-08-07 19:50:08 +0900

    modify UI in main screen
    - add a `upload a template button`

---
+++ screens/mainPage.go
@@ -0,0 +1,56 @@
+package screens
+
+import (
+	"fmt"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
+	"github.com/blackironj/ses-gui/router"
+)
+
+type ListView struct {
+	fyne.Widget
+}
+
+func NewListPage(navigator router.Navigator) (router.Page, error) {
+	sample := widget.NewMultiLineEntry()
+	sample.SetReadOnly(true)
+	/*TODO: 1. Get template list from aws-ses
+	2. make buttons by template name*/
+	var buttons []fyne.CanvasObject
+	for i := 1; i <= 10; i++ {
+		index := i
+		newButton := widget.NewButton(fmt.Sprintf("Button %d", index), func() {
+			//TODO: get a template from aws ses
+			sample.SetText(fmt.Sprintf("Button %d", index))
+			sample.Refresh()
+		})
+		buttons = append(buttons, newButton)
+	}
+
+	uploadBtn := widget.NewButtonWithIcon("Upload a Template", theme.ContentAddIcon(), func() {
+		//TODO: show a file upload window
+		sample.SetText(fmt.Sprintf("Upload button is tapped"))
+		sample.Refresh()
+	})
+
+	templateBtns := widget.NewVScrollContainer(widget.NewVBox(buttons...))
+
+	left := fyne.NewContainerWithLayout(
+		layout.NewBorderLayout(uploadBtn, nil, templateBtns, nil),
+		uploadBtn,
+		templateBtns,
+	)
+	right := widget.NewVScrollContainer(sample)
+
+	content := widget.NewHBox(left, right)
+	return &ListView{
+		content,
+	}, nil
+}
+
+func (page *ListView) BeforeDestroy() {
+
+}

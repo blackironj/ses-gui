81c3a6528eefd1f49297f3acfdafe5cdd070fee5
blackironj <blackironj@gmail.com>
2020-12-14 13:41:22 +0900

    Update UI(btn,entry) for sending a email with template datas

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	fyne.io/fyne v1.4.1
 	github.com/aws/aws-sdk-go v1.35.34
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/satori/go.uuid v1.2.0
 )
--- screens/send.go
+++ screens/send.go
@@ -3,13 +3,21 @@ package screens
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/dialog"
+	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/blackironj/ses-gui/ses"
 )
+
+type orderLabelWithId struct {
+	orderLabel *widget.Label
+	id         string
+}
 
 func makeSendEmailForm(window fyne.Window) *widget.Box {
 	from := widget.NewEntry()
@@ -36,8 +44,60 @@ func makeSendEmailForm(window fyne.Window) *widget.Box {
 		},
 	}
 
+	templateItemBox := widget.NewVBox()
+	orderLabels := []orderLabelWithId{}
+
 	return widget.NewVBox(
 		widget.NewLabelWithStyle("Send a email", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 		form,
+		makeAddTemplateDataBtnWithEntry(templateItemBox, orderLabels),
+		templateItemBox,
 	)
+}
+
+func updateNumLabels(numberLabels []orderLabelWithId) {
+	for i := range numberLabels {
+		numberLabels[i].orderLabel.Text = strconv.Itoa(i+1) + ". "
+	}
+}
+
+func makeAddTemplateDataBtnWithEntry(templateItemBox *widget.Box, orderLabels []orderLabelWithId) *widget.Button {
+	addTemplateDataBtnWithEntry := &widget.Button{
+		Alignment: widget.ButtonAlignLeading,
+		Icon:      theme.ContentAddIcon(),
+		Text:      "Add template data",
+		OnTapped: func() {
+			u1 := uuid.NewV4()
+			label := widget.NewLabel("")
+
+			orderLabels = append(orderLabels, orderLabelWithId{
+				id:         u1.String(),
+				orderLabel: label,
+			})
+
+			entry := widget.NewEntry()
+			delBtn := &widget.Button{
+				Icon: theme.ContentRemoveIcon(),
+				OnTapped: func() {
+					id := u1.String()
+					for i, v := range orderLabels {
+						if id == v.id {
+							orderLabels = append(orderLabels[:i], orderLabels[i+1:]...)
+							updateNumLabels(orderLabels)
+
+							templateItemBox.Children = append(templateItemBox.Children[:i], templateItemBox.Children[i+1:]...)
+							break
+						}
+					}
+					templateItemBox.Refresh()
+				},
+			}
+
+			newItem := widget.NewHBox(label, entry, delBtn)
+			updateNumLabels(orderLabels)
+
+			templateItemBox.Append(newItem)
+		},
+	}
+	return addTemplateDataBtnWithEntry
 }

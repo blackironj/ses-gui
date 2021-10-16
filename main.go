306418554518e6a16ea8211245af939711ecaf51
blackironj <blackironj@gmail.com>
2021-10-16 17:45:40 +0900

    Implement template list UI

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 
 	"github.com/blackironj/ses-gui/screen"
 )
@@ -13,13 +11,7 @@ func main() {
 	a := app.NewWithID("xyz.blackironj.ses-gui")
 	w := a.NewWindow("SES-GUI")
 
-	hello := widget.NewLabel("Hello SES GUI!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+	w.SetContent(screen.MainView())
 
 	screen.AskForAccessToAWS(w, a)
 
